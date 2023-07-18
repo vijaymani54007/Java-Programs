2d87a3877db11b14119426a4b4067facc4106f92
Manikandan.D <111731299+vijaymani54007@users.noreply.github.com>
2023-07-18 19:55:35 +0530

    Add files via upload

---
+++ Family.java
@@ -0,0 +1,12 @@
+class Family 
+{
+	public static void main(String[] args) 
+	{
+		System.out.print("Manikandan");
+		System.out.println("Dhanasekaran");
+		System.out.println("Ananthi");
+		System.out.println("ADhisekar");
+		System.out.println("Arivazhagan");
+		System.out.println("*****************MY LOVABLE FAMILY**********************");
+	}
+}
