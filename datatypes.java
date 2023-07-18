faa4235119abef213648c890aa4f414cbedff105
Manikandan.D <111731299+vijaymani54007@users.noreply.github.com>
2023-07-18 19:56:14 +0530

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,12 @@
+class Datatypes
+{
+	public static void main(String []args)
+	{
+		char mani = 'm';
+		boolean manik = true;
+	    boolean dhanush = false;
+		System.out.println(mani);
+		System.out.println(manik);
+		System.out.println(dhanush);
+	}	
+}
