9a9791b5a83845220dab3340d6cbd3d291558de7
Manikandan.D <111731299+vijaymani54007@users.noreply.github.com>
2023-07-18 19:57:01 +0530

    Add files via upload

---
+++ Timer.java
@@ -0,0 +1,20 @@
+class Timer 
+{
+	public static void main(String[] args)throws Exception 
+	{
+		System.out.println("5");
+		Thread.sleep(1000);
+		System.out.println("4");
+		Thread.sleep(1000);
+		System.out.println("3");
+		Thread.sleep(1000);
+		System.out.println("2");
+		Thread.sleep(1000);
+		System.out.println("1");
+		Thread.sleep(1000);
+		System.out.println("0");
+		Thread.sleep(1000);
+		System.out.println("****************BOOM******************");
+
+	}
+}
