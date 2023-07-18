1178d98fbd596be53f0198f369172e906b144f37
Manikandan.D <111731299+vijaymani54007@users.noreply.github.com>
2023-07-18 19:59:43 +0530

    Add files via upload

---
+++ N3.java
@@ -0,0 +1,20 @@
+class N3 
+{
+	static boolean a= false;
+	static float b=5.3f;
+	static String n="Java";
+	
+	public static void main(String[] args) 
+	{
+        long g=654545415L;
+		System.out.println(g);
+		String n="Tabrez";
+		System.out.println(n);
+		System.out.println(N2.a);
+		N2.a=false;
+		short k=42;
+		System.out.println(k);
+        System.out.println(k=43);
+		System.out.println(k);
+	}
+}
